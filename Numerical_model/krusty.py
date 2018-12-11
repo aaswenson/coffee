d6ffd9b86f5ac33b01cdb2b4355c09bcb4610062
Alexander Swenson <aaswenson@wisc.edu>
2018-12-11 16:59:35 -0600

    vectorize everything, fix error with counting Cp twice

--- Numerical_model/krusty.py
+++ Numerical_model/krusty.py
@@ -12,62 +12,29 @@ PRKE Finite Difference for KRUSTY KRAB REACTOR
 """
 
 class krusty():
-    def __init__(self):
+    # control insertion state, start simulational with step insertion of
+    # reactivity so insert == False
+    insert = False
+    def __init__(self, w_Pu=0):
         self.n0 = 1                     # initial neutron population
-        enrich = 0.93155                # U-235 enrichment
-        Moly_wo = 0.075                 # Moly weight fraction [wt%]
-        rho_cost = 0.60                 # Initial step reactivity cost [$]
+        self.rho_cost = 0.60            # Initial step reactivity cost [$]
+        self.matdat = FuelMat(w_Pu)
         self.reactor()                  # set Physical parameters of system
-        self.nuclear_data(rho_cost)     # set 6-group data
-        self.N235 = self.num_dens(enrich, Moly_wo)     # set U235 number density
-    
-    def nuclear_data(self,rho=0.6):
-        # Fission parameters
-        self.L = float(2.4e-5)                  # mean neutron lifetime
-        self.nu_235 = float(2.57)               # Neutrons per fission, U-235 Fast Fission
-        self.E_fission = float(185) * unit['MeV_J']
-        self.sigma_F = 2 * unit['b_cm']         # Fission cross-section [cm^2]
-        self.n_W = self.E_fission/self.nu_235   # conversion factor from neutron to watts
-        # Decay constant, lambda [sec^-1] (D. Hetrick, Table 1-2, p.11)
-        self.lam = [0.0127, 0.0317, 0.115, 0.311, 1.40, 3.87]
-        # Delayed Neutron Yield [n/fission]
-        self.beta_nu = [0.00063, 0.00351, 0.00310, 0.00672, 0.00211, 0.00043]
-        # Delayed Neutron Fractions
-        self.beta_i = np.divide(self.beta_nu, self.nu_235)
-        self.beta = sum(self.beta_i)            # Total Beta Fraction
-        self.groups = len(self.lam)             # Number of delayed groups
-        # Initial Precursor Vectors
-        self.c0 = [[beta*self.n0/(self.L*lam)] for beta, lam in zip(self.beta_i, self.lam)]
-        self.rho0 = rho*self.beta
-        # # K-factor
-        self.Kf = self.n_W/self.C_th            # k-factor
-
-        # # Neutron speed
-        # E_n = 0.5 * unit['MeV_J']   # averege neutron  energy ~ 500 keV
-        # v_n = np.sqrt(2*E_n/unit['mass_n'])*100 # neutron speed [cm/s]
-
-    def num_dens(self, enr=0.93155, w_Mo = 0.075):
-        """ return number density of U-235"""
-        M_U = (enr/amass['U235'] + (1-enr)/amass['U238'])**-1 # adjusted M Uranium
-        at_Mo = (w_Mo/amass['Mo'])/(w_Mo/amass['Mo'] + (1-w_Mo)/M_U)  # atomic fraction Mo
-        at_U = 1-at_Mo          # atomic fraction U
-        M_UMo = M_U*at_U + amass['Mo']*at_Mo   # adjusted M for U-Mo mixture
-        rho = 17.6              # nominal density 
-        return enr*unit['Av']/amass['U235'] * rho * M_U/M_UMo
-    
+        # reactivity insertion
+        self.rho_insert = self.rho_cost * self.matdat.beta
+ 
     def reactor(self):
         self.volume = float(1940)           # Fuel volume [cc]
+        # reactor fuel and coolant channels start at room temp
+        self.Tf0 = float(293.15)            # Initial Fuel Temperature [K]
+        self.T_ref_rho = self.Tf0
+        self.mass = self.volume*self.dens(self.Tf0)        # [g/cc]
+        
+        # heat pipe geometry
         N_ch = 8                            # Number of coolant channels
         D_ch = 0.0107                         # ID of heat pipe
         A_ch = np.pi*D_ch**2/4              # cross-sectional area of heat pipe
         self.A_chs = N_ch * A_ch            # Total cross-sect. area of heat pipes
-        self.Tf0 = float(20)                # Initial Fuel Temperature [C]
-        self.T_ref_rho = self.Tf0
-        self.insert = False
-        dens_f = self.dens(self.Tf0)        # [g/cc]
-        cp_f = self.cp(self.Tf0)            # [J/g-C]
-        # # Thermal Capacity
-        self.C_th = dens_f*cp_f*self.volume # [g/cc]*[J/g-C]*[cc] = [J/C]
         r_fi = 0.02                         # inner radius, fuel [m]
         r_fo = 0.055                        # outter radius, fuel [m]
         hgt = 0.25                          # Reactor height [m]
@@ -75,42 +42,47 @@ class krusty():
 
     def RungeKutta4(self):        
         
-        times = [315, 1800, 18000]
-        dts   = [1e-3, 0.001, 0.001]
+        times = [300, 1800]
+        dts   = [0.001, 0.001]
         tsec = []
         t0 = 0
         for t, dt in zip(times, dts):
             tsec += list(np.arange(t0, t, dt))
             t0 += t
 
-        rho = [self.rho0]                   # value vectors
-        rho_j0 = rho[0]
-        T_0 = 20
-        rhof = [0]
-        Tf = [self.Tf0]
-        Tc = [self.Tf0]
-        n = [self.n0]
-        c = self.c0
-        order = 4                           # order of approach
-        for ind, t in enumerate(tsec[1:]):
-            dt = t - tsec[ind]
-            #print(dt)
-            str = '{0:.4f} {1:.4e} {2:.4f} {3:.4f}'
-            print(str.format(t, n[-1], rho[-1], Tf[-1]))
-            # initial values
-            n_j0 = n[-1]
-            c_j0 = [x[-1] for x in c]
-            T_j0 = Tf[-1]
-            Tcj0 = Tc[-1]
-            rho_j = self.rho_feedback(T_j0, rho[-1])
+        c   = np.zeros((len(tsec), self.matdat.groups))
+        rho = np.zeros(len(tsec))
+        n   = np.zeros(len(tsec))
+        Tf  = np.zeros(len(tsec))
+        Tc  = np.zeros(len(tsec))
+        p   = np.zeros(len(tsec))
 
+        p[0]   = 0
+        c[0]   = np.array(self.matdat.c0)
+        rho[0] = self.rho_insert
+        n[0]   = self.n0
+        Tf[0]  = self.Tf0
+        Tc[0]  = self.Tf0
+        
+            
+
+        for ind, t in enumerate(tsec[1:]):
+            ind += 1
+            dt = t - tsec[ind-1]
+            # initial values
+            n_j0 = n[ind-1]
+            c_j0 = c[ind-1]
+            T_j0 = Tf[ind-1]
+            Tcj0 = Tc[ind-1]
+            rho_j = self.rho_feedback(T_j0, rho[ind-1])
+            
             # first derivative
             dna = self.fn(n_j0, c_j0, rho_j)
             dca = self.fc(n_j0, c_j0, rho_j)
             dTa = self.fTemp(n_j0, T_j0)
             #
             n_j1 = n_j0 + dna*dt/2
-            c_j1 = [c + dc*dt/2 for c, dc, in zip(c_j0, dca)]
+            c_j1 = np.add(c_j0, np.multiply(dca, dt/2))
             T_j1 = T_j0 + dTa*dt/2
             #
             dnb = self.fn(n_j1, c_j1, rho_j)
@@ -118,7 +90,7 @@ class krusty():
             dTb = self.fTemp(n_j1, T_j1)
             #
             n_j2 = n_j0 + dnb*dt/2
-            c_j2 = [c + dc*dt/2 for c, dc, in zip(c_j0, dcb)]
+            c_j2 = np.add(c_j0, np.multiply(dcb, dt/2))
             T_j2 = T_j0 + dTb*dt/2
             #
             dnc = self.fn(n_j2, c_j2, rho_j)
@@ -126,79 +98,96 @@ class krusty():
             dTc = self.fTemp(n_j2, T_j2)
             #
             n_j3 = n_j0 + dnc*dt
-            c_j3 = [c + dc*dt for c, dc, in zip(c_j0, dcc)]
-            T_j3 = T_j0 + dTc*dt/2
+            c_j3 = np.add(c_j0, np.multiply(dcc, dt))
+            T_j3 = T_j0 + dTc*dt
             #
             dnd = self.fn(n_j3, c_j3, rho_j)
             dcd = self.fc(n_j3, c_j3, rho_j)
             dTd = self.fTemp(n_j3, T_j3)
             #
-            n_j4 = n_j0 + (dna + 2*dnb + 2*dnc + dnd)*dt/6
-            c_j4 = [cj + (a + 2*b + 2*c + d)*dt/6 for cj, a, b,
-                    c, d in zip(c_j0, dca, dcb, dcc, dcd)]
-            T_j4 = T_j0 + (dTa + 2*dTb + 2*dTc + dTd)*dt/6
+            dndt =  (dna + 2*dnb + 2*dnc + dnd)*dt/6
+            dcdt =  np.add(np.add(np.add(dca, 2*dcb), 2*dcc), dcd)*dt/6
+            dTdt =  (dTa + 2*dTb + 2*dTc + dTd)*dt/6
             
-            n.append(n_j4)
-            Tf.append(T_j4)
-            for i in range(self.groups):
-                c[i].append(c_j4[i])    
-            rho.append(rho_j)
+            n_1 = n_j0 + (dna + 2*dnb + 2*dnc + dnd)*dt/6
+            c_1 = c_j0 + np.add(np.add(np.add(dca, 2*dcb), 2*dcc), dcd)*dt/6
+            T_1 = T_j0 + (dTa + 2*dTb + 2*dTc + dTd)*dt/6
+            
+            n[ind]  = n_1
+            Tf[ind] = T_1
+            p[ind]  = n_1 * self.matdat.n_W
+            rho[ind] = rho_j
+            c[ind] = np.array(c_1)
+            
+#            if ind % 5000 == 0 and ind > 0:
+#                self.plot_results(tsec[:ind],
+#                                  n[:ind],
+#                                  rho[:ind],
+#                                  Tf[:ind],
+#                                  c[:ind])
+            
+            str = '{0:.4f} {1:.4e} {2:.4f} {3:.4f}'
+            print(str.format(t, p[ind], rho[ind], T_1))
 
         self.plot_results(tsec,n,rho,Tf,c)
     
     def fn(self, n, c, rho):
         """ Neutron derivative function """
-        return (rho-self.beta)/self.L*n + np.dot(c, self.lam)
+        return (rho-self.matdat.beta)/self.matdat.L*n + np.dot(c, self.matdat.lam)
 
     def fc(self, n, c, rho):
         """ 6-group precursor derivative function """
-        cs = []
-        for ind, ci in enumerate(c):
-            cs.append(self.beta_i[ind] / self.L * n - self.lam[ind] * ci)
+        
+        cs = np.add(np.divide(np.multiply(n, self.matdat.beta_i), self.matdat.L), 
+                      np.multiply(-self.matdat.lam, c))
+        
         return cs
 
     def fci(self, group, n, c, rho):
         """ Single group (of 6) precursor derivative function """
-        return self.beta_i[group] / self.L * n - self.lam[group] * c
-
-    # def fcool(self,)
+        return self.matdat.beta_i[group] / self.matdat.L * n - self.matdat.lam[group] * c
 
     def fTemp(self, n, T):
         """ Fuel Temperautre function """
-        # return (self.H0*n/(self.dens(T)*self.cp(T) * self.volume))
-        P_fuel = self.Kf * n                # Reactor power
+        P_fuel = n*self.matdat.n_W            # Reactor power
         deltaT = T/20                         # Temp. diff. from fuel to coolant(bulk)
-        h_bar = 40000                         # [W/m^2-K]
+        h_bar = 20000                         # [W/m^2-K]
         R_f = self.R_f0/self.kcond(T)
         R_conv = 1/(h_bar*self.A_chs)
         R_total = R_f + R_conv
-        if T <= 100:
+        
+        C = self.cp(T) * self.mass
+        
+        if T <= 373.15:
             Q_out = 0
         else:
-            deltaT = 20
+            deltaT = 373
             Q_out = deltaT/R_total
-        return (P_fuel - Q_out)/self.C_th
-
-    
-    def rho_feedback(self,T, rho1):
         
-        if T > 350 and self.insert == False:
-            self.insert = True
-            self.rho0 = rho1
-            self.T_ref_rho = 350
+        return (P_fuel - Q_out)/C
 
-        return self.rho0 + self.RTC(T)*(T-self.T_ref_rho)
+    def rho_feedback(self,T, rho1):
+        """Calculate reactivity response to temperature.
+        """
+        if T > 623.15 and self.insert == False:
+            self.insert = True
+            self.rho_insert = rho1
+            self.T_ref_rho = 623.15
+
+        return self.rho_insert + self.RTC(T)*(T-self.T_ref_rho)
 
     def RTC(self, T):
         """ Fuel Temperautre Reactivity Coeficient [K^-1] """
-        alpha = self.beta*(-7.3e-9*(T+unit['C_K'])**2 -7.58e-5*(T+unit['C_K']) -0.113)/100
+        alpha = self.matdat.beta*(-7.3e-9*(T)**2 -7.58e-5*(T) -0.113)/100
         return alpha
 
     def cp(self, T):
         """ Fuel Specific Heat [J/g-C] """
         # Eq.1 (INL/EXT-10-19373 Thermophysical Properties of U-10Mo Alloy)
         # Range: 100 < T < 1000 [C]
-        return float(0.137 + 5.12e-5*T + 1.99e-8*T**2)
+        T = T - unit['C_K']
+
+        return float(0.137 + 5.12e-5*T + 1.99e-8*T**2) 
 
     def dens(self, T):
         """ Fuel Density [g/cc] """
@@ -240,13 +229,15 @@ class krusty():
         plt.savefig('temp.png')
         # Reactor Power
         plt.figure()
-        power = np.multiply(n,self.n_W)
+        power = np.multiply(n,self.matdat.n_W)
         plt.plot(time,power,label = 'watts')
         plt.xlabel('Time [sec]')
         plt.ylabel('P(t)')
         plt.yscale('log')
         plt.savefig('power.png')
-
+        
+        plt.clf()
+        plt.close()
 
 def main():
     start = time.time()
--- Numerical_model/units.py
+++ Numerical_model/units.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
- 
+import numpy as np
+
 unit = {'Av'    : float(6.0225e23),
          'MeV_J' : float(1.6021892e-13),
          'boltz' : float(1.380662e-23),
@@ -17,7 +17,68 @@ unit = {'Av'    : float(6.0225e23),
 
 # # Atomic Masses
 amass = {'U235': float(235.0439299),
-        'U238': float(238.05078826),
-        'Mo'  : float(95.95)
+         'U238': float(238.05078826),
+         'Pu239' : float(239.0521634),
+         'Mo'  : float(95.95)
         }
 
+dens = {'U235' : float(19.1),
+        'Pu239' : float(19.86)
+       }
+
+
+class FuelMat:
+    # Decay constant, lambda [sec^-1] (D. Hetrick, Table 1-2, p.11)
+    lams = {'U235'  : np.array([0.0127, 0.0317, 0.115, 0.311, 1.40, 3.87]),
+            'Pu239' : np.array([0.0127, 0.0317, 0.115, 0.311, 1.40, 3.87])
+           }
+    # Delayed Neutron Yield [n/fission]
+    beta_nu = {'U235'  : np.array([0.00063, 0.00351, 0.00310, 0.00672, 0.00211, 0.00043]),
+               'Pu239' : np.array([0.00063, 0.00351, 0.00310, 0.00672, 0.00211, 0.00043])
+              }
+    
+    nuc_data = {'U235'  : {'efiss' : 185,   'nu' : 2.57},
+                'Pu239' : {'efiss' : 211.5, 'nu' : 2.88}
+               }
+
+    def __init__(self, Pufrac, enrich=0.93155):
+
+        self.pu_mfrac = Pufrac
+        self.enrich = enrich
+
+        self.mix_mats()
+        self.nuc_data_make()
+
+    def mix_mats(self, w_Pu=0, enr=0.93155, w_Mo=0.075):
+        """ return number density of U-235"""
+        
+        mfrac_235 = (1 - w_Pu)*enr
+        
+        N235 = mfrac_235 * dens['U235'] * unit['Av'] / amass['U235']
+        N239 = w_Pu * dens['Pu239'] * unit['Av'] / amass['U235']
+        
+        Ntot = N235 + N239
+        self.afrac = {'U235' : N235/ Ntot, 'Pu239' : N239 / Ntot}
+        
+    def nuc_data_make(self):
+        # Fission parameters
+        self.L = float(2.4e-5)                  # mean neutron lifetime
+        self.E_fission = 0
+        self.nu = 0
+        self.beta_i = np.zeros(6)
+        self.lam = np.zeros(6)
+        for fuel in self.nuc_data:
+            self.nu += self.nuc_data[fuel]['nu']*self.afrac[fuel]
+            self.E_fission += self.nuc_data[fuel]['efiss'] * self.afrac[fuel] *\
+                              unit['MeV_J']
+            self.beta_i += self.beta_nu[fuel] * self.afrac[fuel] /\
+                                                self.nuc_data[fuel]['nu']
+            self.lam += self.lams[fuel] * self.afrac[fuel]
+        
+
+        self.n_W = self.E_fission/self.nu   # conversion factor from neutron to watts
+        # Delayed Neutron Fractions
+        self.beta = sum(self.beta_i)            # Total Beta Fraction
+        self.groups = len(self.lam)             # Number of delayed groups
+        # Initial Precursor Vectors
+        self.c0 = np.array([beta/(self.L*lam) for beta, lam in zip(self.beta_i, self.lam)])
